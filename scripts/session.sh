57d2ab67002847ff47e3e7a7d3db1588b88ca95c
leon <king0980692@gmail.com>
2026-01-25 13:03:08 +0800

    fix: new session bug

--- scripts/session.sh
+++ scripts/session.sh
@@ -14,9 +14,21 @@ if [[ -z "$TMUX_FZF_SWITCH_CURRENT" ]]; then
     sessions=$(echo "$sessions" | grep -v "^$current_session: ")
 fi
 
+single_select_opts="$TMUX_FZF_OPTIONS"
+single_select_opts=$(echo "$single_select_opts" | sed -E 's/(^|[[:space:]])(-m|--multi)([[:space:]]|$)/\1/g')
+single_select_opts=$(echo "$single_select_opts" | sed -E 's/[[:space:]]+/ /g' | sed -E 's/^ //; s/ $//')
+multi_select_opts="$TMUX_FZF_OPTIONS"
+if ! echo "$multi_select_opts" | grep -Eq '\\b(-m|--multi)( |=|$)'; then
+    if [ -z "$multi_select_opts" ]; then
+        multi_select_opts="-m"
+    else
+        multi_select_opts="$multi_select_opts -m"
+    fi
+fi
+
 FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --header='Select an action.'"
 if [[ -z "$1" ]]; then
-    action=$(printf "switch\nnew\nrename\ndetach\nkill\n[cancel]" | eval "$TMUX_FZF_BIN $TMUX_FZF_OPTIONS")
+    action=$(printf "switch\nnew\nrename\ndetach\nkill\n[cancel]" | eval "$TMUX_FZF_BIN $single_select_opts")
 else
     action="$1"
 fi
@@ -29,30 +41,20 @@ if [[ "$action" != "detach" ]]; then
         FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --header='Select target session.'"
     fi
     if [[ "$action" == "switch" ]]; then
-        target_origin=$(printf "%s\n[cancel]" "$sessions" | eval "$TMUX_FZF_BIN $TMUX_FZF_OPTIONS $TMUX_FZF_PREVIEW_SESSION_OPTIONS")
+        target_origin=$(printf "%s\n[cancel]" "$sessions" | eval "$TMUX_FZF_BIN $single_select_opts $TMUX_FZF_PREVIEW_SESSION_OPTIONS")
     elif [[ "$action" != "new" ]]; then
-        target_origin=$(printf "[current]\n%s\n[cancel]" "$sessions" | eval "$TMUX_FZF_BIN $TMUX_FZF_OPTIONS $TMUX_FZF_PREVIEW_SESSION_OPTIONS")
+        target_origin=$(printf "[current]\n%s\n[cancel]" "$sessions" | eval "$TMUX_FZF_BIN $single_select_opts $TMUX_FZF_PREVIEW_SESSION_OPTIONS")
         target_origin=$(echo "$target_origin" | sed -E "s/\[current\]/$current_session:/")
     fi
-    if [[ "$action" == "new" || "$action" == "rename" ]]; then
-        # Create a unique temporary file for session name
-        tmp_file=$(mktemp /tmp/tmux_fzf_session_name.XXXXXX)
-        tmux display-popup -h 50% -w 50% -E "bash -c 'printf \"Session Name: \" && read session_name && echo \"\$session_name\" > $tmp_file'"
-        session_name=$(cat "$tmp_file")
-        rm -f "$tmp_file"
-        if [ -z "$session_name" ]; then
-            exit
-        fi
-        if [[ "$action" == "new" ]]; then
-            tmux new-session -d -s "$session_name" && tmux switch-client -t "$session_name"
-            exit
-        fi
+    if [[ "$action" == "new" ]]; then
+        tmux command-prompt -p "New session name:" "new-session -d -s '%%' \\; switch-client -t '%%'"
+        exit
     fi
 else
     tmux_attached_sessions=$(tmux list-sessions | grep 'attached' | grep -o '^[[:alpha:][:digit:]_-]*:' | sed 's/.$//g')
     sessions=$(echo "$sessions" | grep "^$tmux_attached_sessions")
     FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --header='Select target session(s). Press TAB to mark multiple items.'"
-    target_origin=$(printf "[current]\n%s\n[cancel]" "$sessions" | eval "$TMUX_FZF_BIN $TMUX_FZF_OPTIONS $TMUX_FZF_PREVIEW_SESSION_OPTIONS")
+    target_origin=$(printf "[current]\n%s\n[cancel]" "$sessions" | eval "$TMUX_FZF_BIN $multi_select_opts $TMUX_FZF_PREVIEW_SESSION_OPTIONS")
     target_origin=$(echo "$target_origin" | sed -E "s/\[current\]/$current_session:/")
 fi
 [[ "$target_origin" == "[cancel]" || -z "$target_origin" ]] && exit
@@ -64,5 +66,5 @@ elif [[ "$action" == "detach" ]]; then
 elif [[ "$action" == "kill" ]]; then
     echo "$target" | sort -r | xargs -I{} tmux kill-session -t "{}"
 elif [[ "$action" == "rename" ]]; then
-    tmux rename-session -t "$target" "$session_name"
+    tmux command-prompt -I "$target" -p "Rename session:" "rename-session -t '$target' '%%'"
 fi
